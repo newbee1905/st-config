e5224c718da4061ecf55269b16179d1f37719a2c
newbee1905 <beenewminh@outlook.com>
2021-09-14 14:32:22 +0700

    Add ligatures patch

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 0.8.4
 # Customize below to fit your system
 
 # paths
-PREFIX = ~/.local
+PREFIX = /home/newbee/.local
 MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
@@ -14,22 +14,25 @@ PKG_CONFIG = pkg-config
 
 # includes and libs
 INCS = -I$(X11INC) \
-       `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2`
+			 `$(PKG_CONFIG) --cflags fontconfig` \
+			 `$(PKG_CONFIG) --cflags freetype2` \
+			 `$(PKG_CONFIG) --cflags harfbuzz` 
+
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
-       `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2`
+			 `$(PKG_CONFIG) --libs fontconfig` \
+			 `$(PKG_CONFIG) --libs freetype2` \
+			 `$(PKG_CONFIG) --libs harfbuzz`
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS) -Ofast
+STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS) -Ofast 
 STLDFLAGS = $(LIBS) $(LDFLAGS)
 
 # OpenBSD:
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `$(PKG_CONFIG) --libs fontconfig` \
-#       `$(PKG_CONFIG) --libs freetype2`
+#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \			 
+#				`$(PKG_CONFIG) --libs fontconfig` \
+#				`$(PKG_CONFIG) --libs freetype2`
 
-# compiler and linker
-# CC = c99
+#compiler and linker
+CC = gcc
